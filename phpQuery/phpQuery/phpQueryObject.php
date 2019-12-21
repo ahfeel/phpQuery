aa0c33e5022de1efa3088de5c40caf875fd6b14f
ahfeel <ahfeel@gmail.com>
2019-12-21 19:47:46 +0100

    * Typo

--- phpQuery/phpQuery/phpQueryObject.php
+++ phpQuery/phpQuery/phpQueryObject.php
@@ -1051,7 +1051,7 @@ return pq($node)->siblings()->size() == 0 ? $node : null;
 				if (! $param)
 					break;
 					// nth-child(n+b) to nth-child(1n+b)
-				if ($param[0]} == 'n')
+				if ($param[0] == 'n')
 					$param = '1'.$param;
 				// :nth-child(index/even/odd/equation)
 				if ($param == 'even' || $param == 'odd')
